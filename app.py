7bd2078230da9ac0c6abc949890b5bde47363cc7
Karol Behrendt <karol.behrendt.zbigniew@gmail.com>
2023-07-02 16:52:14 +0200

    Wersja 1.0

--- app.py
+++ app.py
@@ -23,55 +23,82 @@ gracz_2=talia_kart[:index]
 
 new_t1=[]
 new_t2=[]
-while(index>0):
+while True:
+
     if (len(gracz_1)!=0) and (len(gracz_2)!=0):
-        for karta_gracza_1, karta_gracza_2 in zip(gracz_1, gracz_2, new_t1, new_t2):
+        for karta_gracza_1, karta_gracza_2 in zip(gracz_1, gracz_2):
             if karta_gracza_1>karta_gracza_2:
                 win_k=karta_gracza_2
                 gracz_1.remove(karta_gracza_1)
                 gracz_2.remove(karta_gracza_2)
-                new_t1.append(win_k)
-
+                #gracz_1.append(karta_gracza_2)
+                new_t1.append(karta_gracza_2)
             elif  karta_gracza_1<karta_gracza_2:
                 win_k=karta_gracza_1
                 gracz_1.remove(karta_gracza_1)
                 gracz_2.remove(karta_gracza_2)
-                new_t2.append(win_k)
-
+                #gracz_2.append(karta_gracza_1)
+                new_t2.append(karta_gracza_1)
             else:
                 gracz_1.remove(karta_gracza_1)
                 gracz_2.remove(karta_gracza_2)
                 continue
-    elif (len(gracz_1)==0 and len(new_t1)!=0) and (len(gracz_2)==0 and len(new_t2)!=0):
-        for karta_gracza_1, karta_gracza_2 in zip(gracz_1, gracz_2, new_t1, new_t2):
+
+    elif ((len(gracz_1)!=0) and (len(new_t2)!=0)):
+        for karta_gracza_1, karta_gracza_2 in zip( gracz_1, new_t2):
             if karta_gracza_1>karta_gracza_2:
-                win_k=karta_gracza_2
-                new_t1.remove(karta_gracza_1)
+                gracz_1.remove(karta_gracza_1)
                 new_t2.remove(karta_gracza_2)
-                gracz_1.append(win_k)
-
+                new_t1.append(karta_gracza_2)
             elif  karta_gracza_1<karta_gracza_2:
-                win_k=karta_gracza_1
-                new_t1.remove(karta_gracza_1)
+                gracz_1.remove(karta_gracza_1)
                 new_t2.remove(karta_gracza_2)
-                gracz_2.append(win_k)
-
+                gracz_2.append(karta_gracza_1)
             else:
-                new_t1.remove(karta_gracza_1)
+                gracz_1.remove(karta_gracza_1)
                 new_t2.remove(karta_gracza_2)
                 continue
 
-
-
+    elif ((len(gracz_2)!=0) and (len(new_t1)!=0)):
+        for karta_gracza_1, karta_gracza_2 in zip(new_t1,gracz_2):
+            if karta_gracza_1>karta_gracza_2:
+                new_t1.remove(karta_gracza_1)
+                gracz_2.remove(karta_gracza_2)
+                gracz_1.append(karta_gracza_2)
+            elif  karta_gracza_1<karta_gracza_2:
+                new_t1.remove(karta_gracza_1)
+                gracz_2.remove(karta_gracza_2)
+                new_t2.append(karta_gracza_1)
+            else:
+                new_t1.remove(karta_gracza_1)
+                gracz_2.remove(karta_gracza_2)
+                continue
     
-    index-=1
-print(len(gracz_1))
-print(len(gracz_2))
-print(len(new_t1))
-print(len(new_t2))
-if len(gracz_2)==0:
+    elif (len(gracz_1)==0) and (len(gracz_2)==0):   
+        for karta_gracza_1, karta_gracza_2 in zip( new_t1, new_t2):
+            if karta_gracza_1>karta_gracza_2:
+                new_t1.remove(karta_gracza_1)
+                new_t2.remove(karta_gracza_2)
+                gracz_1.append(karta_gracza_2)
+            elif  karta_gracza_1<karta_gracza_2:
+                new_t1.remove(karta_gracza_1)
+                new_t2.remove(karta_gracza_2)
+                gracz_2.append(karta_gracza_1)
+            else:
+                new_t1.remove(karta_gracza_1)
+                new_t2.remove(karta_gracza_2)
+                continue 
+    elif ((len(gracz_1)==0 and len(new_t1)==0)) or (((len(gracz_2)==0) and len(new_t2)==0)):
+        break
+    else:
+        print("Erorr")
+        break
+    
+print("Ilość kart gracza 1:",(len(gracz_1)+len(new_t1)))
+print("Ilość kart gracza 2:",(len(gracz_2)+len(new_t2)))
+if len(gracz_2)==0 and len(new_t2)==0:
      print("Wygrał gracz 1")
-elif len(gracz_1)==0 :
+elif len(gracz_1)==0 and len(new_t1)==0:
     print("Wygrał gracz 2")
   
          
