95246b59edc871dd725e44edac9b4b2727da5218
Karol Behrendt <karol.behrendt.zbigniew@gmail.com>
2023-07-10 13:01:26 +0200

    Dodawanie większej ilości gier działa Czyszczenie exela takze Zostało dodawanie graczy i zeby wyswietlały sie ich imiona ;-;

--- PLAYERS.py
+++ PLAYERS.py
@@ -16,17 +16,4 @@ class Players:
         self.number_of_players=number_of_players
         self.number_of_games=number_of_games
 
-number_of_players=int(input("Enter the numbers of players: "))
-number_of_games=int(input("Enter the numbers of games: "))
 
-players=Players(number_of_players,number_of_games)
-
-list_of_players=[]
-while  number_of_players>0: 
-    player_name=input("Enter you'r name: ")
-    list_of_players.append(player_name)
-    number_of_players-=1
-    if players.check_names(list_of_players):
-        players.name(list_of_players)
-    else:
-            print("Duplicate names found. Please provide different names.")
