a34a36e6ed5f5be200f47847a64e64aada1e5c60
Karol Behrendt <karol.behrendt.zbigniew@gmail.com>
2023-09-03 20:05:31 +0200

    doopa
    doopa

--- PLAYERS.py
+++ PLAYERS.py
@@ -1,23 +1,14 @@
 class PLAYER_BASE:
     def __init__(self,number_of_players, number_of_games,number_of_cards):
+        self.players = []
         self.number_of_players=number_of_players
         self.number_of_games=number_of_games
         self.number_of_cards=number_of_cards
-    def give_name(self):
+    def give_number_of_players(self):
         return self.number_of_players
-        # number_of_players=self.number_of_players
-        # list_of_names=[]
-        # while number_of_players>0:
-        #     name= input("Enter you'r name: ")
-        #     list_of_names.append(name)
-        # return list_of_names
     def give_cards(self):
         return self.number_of_cards
     def give_pack(self):
         return self.number_of_cards*2
     def give_games(self):
         return self.number_of_games
-
-
-
-
--- WAR.py
+++ WAR.py
@@ -1,4 +1,8 @@
 class War:
+    def __init__(self,number_of_cards,player_pack,number_of_players):
+        self.number_of_cards=number_of_cards
+        self.player_pack=player_pack                
+        self.number_of_players=number_of_players 
     def shuffle(self):
         number_of_cards=self.number_of_cards
         pack=[]
@@ -6,12 +10,7 @@ class War:
             for card_suit in range(4):
                     pack.append(card+1)  
         self.pack_after_shuffle=pack
-        return(pack)
-    def create_player(self,number):
-        self.number_of_games=number
-        while number>0:
-            pass
-        pass    
+        return(pack)   
     def deal(self):
         pack=self.pack_after_shuffle
         index=self.player_pack
@@ -21,14 +20,15 @@ class War:
         player_2=pack[:index]
         self.player_pack_1=player_1
         self.player_pack_2=player_2
-    def game(self):
+        pass
+    def game_two_players(self):
         import random
         player_1=self.player_pack_1
         player_2=self.player_pack_2
         new_player_1=[]
         new_player_2=[]
         while True:
-            if (len(player_1)!=0) and (len(player_2)!=0 and ((len(new_player_1)==0) or (len(new_player_2)==0))):
+            if (len(player_1)!=0 and len(player_2)!=0 and (len(new_player_1)==0 or len(new_player_2)==0)):
                 for card_player_1, card_player_2 in zip(player_1, player_2):
                     if card_player_1>card_player_2:
                             player_1.remove(card_player_1)
@@ -103,43 +103,108 @@ class War:
         self.end_player_new_pack_1=new_player_1
         self.end_player_new_pack_2=new_player_2
     def display(self):
-        player_1=self.end_player_1
-        player_2=self.end_player_2
-        new_player_1=self.end_player_new_pack_1
-        new_player_2=self.end_player_new_pack_2
-        print("Number of cards for player 1: ",(len(player_1)+len(new_player_1)))
-        print("Number of cards for player 2: ",(len(player_2)+len(new_player_2)))
-        if len(player_1)!=0 and len(new_player_1)!=0 and len(player_2)==0 and len(new_player_2)==0:
-            print("Player 1 won")
-        elif len(player_1)==0 and len(new_player_1)==0 and len(player_2)!=0 and len(new_player_2)!=0:
-            print("Player 2 won")
-        elif len(player_1)==0 and len(new_player_1)==0 and len(player_2)==0 and len(new_player_2)==0:
-            print("DRAW")
+        if(self.number_of_players==2):
+            player_1=self.end_player_1
+            player_2=self.end_player_2
+            new_player_1=self.end_player_new_pack_1
+            new_player_2=self.end_player_new_pack_2
+            print("Number of cards for player 1: ",(len(player_1)+len(new_player_1)))
+            print("Number of cards for player 2: ",(len(player_2)+len(new_player_2)))
+            if len(player_1)!=0 and len(new_player_1)!=0 and len(player_2)==0 and len(new_player_2)==0:
+                print("Player 1 won")
+            elif len(player_1)==0 and len(new_player_1)==0 and len(player_2)!=0 and len(new_player_2)!=0:
+                print("Player 2 won")
+        elif(self.number_of_players==3):
+            player_1=self.end_player_1
+            player_2=self.end_player_2
+            new_player_1=self.end_player_new_pack_1
+            new_player_2=self.end_player_new_pack_2
+            print("Number of cards for player 1: ",(len(player_1)+len(new_player_1)))
+            print("Number of cards for player 2: ",(len(player_2)+len(new_player_2)))
+            if len(player_1)!=0 and len(new_player_1)!=0 and len(player_2)==0 and len(new_player_2)==0:
+                print("Player 1 won")
+            elif len(player_1)==0 and len(new_player_1)==0 and len(player_2)!=0 and len(new_player_2)!=0:
+                print("Player 2 won")
+        elif(self.number_of_players==4):
+            player_1=self.end_player_1
+            player_2=self.end_player_2
+            new_player_1=self.end_player_new_pack_1
+            new_player_2=self.end_player_new_pack_2
+            print("Number of cards for player 1: ",(len(player_1)+len(new_player_1)))
+            print("Number of cards for player 2: ",(len(player_2)+len(new_player_2)))
+            if len(player_1)!=0 and len(new_player_1)!=0 and len(player_2)==0 and len(new_player_2)==0:
+                print("Player 1 won")
+            elif len(player_1)==0 and len(new_player_1)==0 and len(player_2)!=0 and len(new_player_2)!=0:
+                print("Player 2 won")
+        elif(self.number_of_players==5):
+            player_1=self.end_player_1
+            player_2=self.end_player_2
+            new_player_1=self.end_player_new_pack_1
+            new_player_2=self.end_player_new_pack_2
+            print("Number of cards for player 1: ",(len(player_1)+len(new_player_1)))
+            print("Number of cards for player 2: ",(len(player_2)+len(new_player_2)))
+            if len(player_1)!=0 and len(new_player_1)!=0 and len(player_2)==0 and len(new_player_2)==0:
+                print("Player 1 won")
+            elif len(player_1)==0 and len(new_player_1)==0 and len(player_2)!=0 and len(new_player_2)!=0:
+                print("Player 2 won")
+        elif(self.number_of_players==6):
+            player_1=self.end_player_1
+            player_2=self.end_player_2
+            new_player_1=self.end_player_new_pack_1
+            new_player_2=self.end_player_new_pack_2
+            print("Number of cards for player 1: ",(len(player_1)+len(new_player_1)))
+            print("Number of cards for player 2: ",(len(player_2)+len(new_player_2)))
+            if len(player_1)!=0 and len(new_player_1)!=0 and len(player_2)==0 and len(new_player_2)==0:
+                print("Player 1 won")
+            elif len(player_1)==0 and len(new_player_1)==0 and len(player_2)!=0 and len(new_player_2)!=0:
+                print("Player 2 won")
     def excel(self):
         import openpyxl
         workbook = openpyxl.load_workbook('table.xlsx')
         sheet = workbook.active
         score_1=len(self.end_player_1+self.end_player_new_pack_1)
         score_2=len(self.end_player_2+self.end_player_new_pack_2)
+        score_3=len(self.end_player_3+self.end_player_new_pack_3)
+        score_4=len(self.end_player_4+self.end_player_new_pack_4)
+        score_5=len(self.end_player_5+self.end_player_new_pack_5)
+        score_6=len(self.end_player_6+self.end_player_new_pack_6)
         sheet["A1"]="Player 1 score" 
         sheet["B1"]="Player 2 score"
-        data=[score_1,score_2]
+        sheet["C1"]="Player 3 score"
+        sheet["D1"]="Player 4 score"
+        sheet["E1"]="Player 5 score"
+        sheet["F1"]="Player 6 score"
+        data=[score_1,score_2,score_3,score_4,score_5,score_6]
         sheet.append(data)
         workbook.save('table.xlsx')
     def clear(self):
         import openpyxl
         workbook = openpyxl.load_workbook('table.xlsx')
         sheet = workbook.active
-        for i in range(2):
+        for i in range(self.number_of_players):
             column_index = i
             for row in sheet.iter_rows(min_row=2, min_col=column_index, max_col=column_index):
                 for cell in row:
                     cell.value = None
         workbook.save('table.xlsx')
-    def __init__(self,number_of_cards,player_pack,list_of_names):
-        self.number_of_cards=number_of_cards
-        self.player_pack=player_pack                
-        self.list_of_names=list_of_names              
+             
+class Condition:
+     def __init__(self,number_of_players):
+          self.number_of_players=number_of_players
+     def check(self):
+          if(self.number_of_players==2):
+               return self.check_value
+          elif(self.number_of_players==3):
+               pass
+          elif(self.number_of_players==4):
+               pass
+          elif(self.number_of_players==5):
+               pass
+          elif(self.number_of_players==6):
+               pass
+
+
+
 
 
 
